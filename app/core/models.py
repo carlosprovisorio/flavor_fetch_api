06fc8f07c37dc13dd12781c6baafc9ea4bf5b8ad
Carlos <carlosprovisorio@icloud.com>
2024-10-03 16:03:56 -0400

    Create User manager class

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,18 @@ from django.contrib.auth.models import (
 )
 
 
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
 
