49a6e797c245ff7ab27d1541f26f79c644fbc152
Carlos <carlosprovisorio@icloud.com>
2024-10-03 15:48:47 -0400

    Create user model under the models.py

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,20 @@
-from django.db import models  # noqa
+"""Database models."""
 
-# Create your models here.
+from os import name
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.EmailField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    USERNAME = "email"
