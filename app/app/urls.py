a5422b8d0662eba71ab19b7196f283c6491fbadb
Carlos <carlosprovisorio@icloud.com>
2024-10-09 14:45:41 -0400

    Connect view into the main app

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -30,4 +30,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path("api/user/", include("users.urls")),
 ]
