6dc5b7f20f7de378b28d9c0fce4717e996fd0778
Carlos <carlosprovisorio@icloud.com>
2024-10-11 15:16:17 -0400

    Implement manage user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -158,4 +158,4 @@ class PrivateUserApiTests(TestCase):
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload["name"])
         self.assertTrue(self.user.check_password(payload["password"]))
-        self.assertEqual(res.status_code, status.HTTP_200_Ok)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
