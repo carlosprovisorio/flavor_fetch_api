8bb514034636d0b101d15928fde96b19ddaaac58
Carlos <carlosprovisorio@icloud.com>
2024-10-11 14:17:12 -0400

    Write tests for manage user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -129,3 +129,33 @@ class PrivateUserApiTests(TestCase):
 
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user."""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            res.data,
+            {
+                "name": self.user.name,
+                "email": self.user.email,
+            },
+        )
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me URL."""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user."""
+        payload = {"name": "Updated name", "password": "newpassword123"}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload["name"])
+        self.assertTrue(self.user.check_password(payload["password"]))
+        self.assertEqual(res.status_code, status.HTTP_200_Ok)
