b8e1bf12ecf0ed0b9e85565c58127d152d087dd0
Carlos <carlosprovisorio@icloud.com>
2024-10-20 18:27:58 -0400

    Create Public class and method to handle unauthenticated user

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -24,3 +24,16 @@ TAGS_URL = reverse("recipe:tag-list")
 def create_user(email="user@example.com", password="testpass123"):
     """Create and return a user."""
     return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PublicTagsApiTests(TestCase):
+    """Test unauthenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required for retrieving tags."""
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
