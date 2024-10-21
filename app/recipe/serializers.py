2b7cf1e4ad5541b0ed8960ce9b3731f72ac33428
Carlos <carlosprovisorio@icloud.com>
2024-10-21 16:11:13 -0400

    Add tags serializer and urls

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -22,7 +22,7 @@ and proper formatting.
 
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -45,3 +45,12 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ["description"]
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects."""
+
+    class Meta:
+        model = Tag
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
