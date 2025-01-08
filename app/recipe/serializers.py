1df126ee692bf40564b763291f3a1a65e04911c4
Carlos <carlosprovisorio@icloud.com>
2025-01-08 11:24:04 -0500

    Fixed the variable ingredients that was not following the lowercase convention

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -33,7 +33,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
     tags = TagSerializer(many=True, required=False)
-    Ingredients = IngredientSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
