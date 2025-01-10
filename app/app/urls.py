3bc99701a4c29d8d774e8645c4c7c4371decab6b
Carlos <carlosprovisorio@icloud.com>
2025-01-10 14:25:52 -0500

    Modify recipe model, run migrations and fix missing slash on the seetings file

--- app/app/urls.py
+++ app/app/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path("api/recipe/", include("recipe.urls")),
 ]
 
-if setting.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
