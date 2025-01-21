62c35d629506e41ed81f26fda760033bba8c7aab
Carlos <carlosprovisorio@icloud.com>
2025-01-21 17:35:00 -0500

    Add extend schema view decorator

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -103,6 +103,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "assigned_only",
+                OpenApiTypes.INT,
+                enum=[0, 1],
+                description="Filter by items assigned to recipes.",
+            ),
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(
     mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
